5a1099136d811a4dbb05d9994616b2e8f43c93c5
Inshalfatima22 <inshal.fatima.st@gmail.com>
2026-05-17 20:33:46 +0400

    add login system

--- Week 10/app.py
+++ Week 10/app.py
@@ -1,25 +1,52 @@
 import tkinter as tk
-# login reading from file
+from tkinter import messagebox
+
+USERS_FILE = "users.txt"
+
 def read_file():
-    
-    pass
+    users = {}
+    try:
+        with open(USERS_FILE, "r") as f:
+            for line in f:
+                user, pwd = line.strip().split(",")
+                users[user] = pwd
+    except FileNotFoundError:
+        pass
+    return users
 
-def write_file():
-    pass
-
+def write_file(username, password):
+    with open(USERS_FILE, "a") as f:
+        f.write(f"{username},{password}\n")
 
 def login():
-    pass
+    users = read_file()
+    u, p = entry_user.get(), entry_pass.get()
+    if users.get(u) == p:
+        messagebox.showinfo("Success", f"Welcome, {u}!")
+    else:
+        messagebox.showerror("Error", "Invalid credentials.")
 
 def signup():
-    pass    
-
-def main():
-    pass
+    users = read_file()
+    u, p = entry_user.get(), entry_pass.get()
+    if u in users:
+        messagebox.showerror("Error", "User already exists.")
+    else:
+        write_file(u, p)
+        messagebox.showinfo("Success", "Account created!")
 
 root = tk.Tk()
-root.title("Login System")    
+root.title("Login System")
 
+tk.Label(root, text="Username").pack()
+entry_user = tk.Entry(root)
+entry_user.pack()
 
+tk.Label(root, text="Password").pack()
+entry_pass = tk.Entry(root, show="*")
+entry_pass.pack()
+
+tk.Button(root, text="Login", command=login).pack(pady=5)
+tk.Button(root, text="Sign Up", command=signup).pack()
 
 root.mainloop()
